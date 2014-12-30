acd5ce9ac24cb4d2aeee94f66a8ea56ef47c4af9
Dide van Berkel <didevanberkel@MacBook-Air-van-Dide.local>
2014-12-30 12:22:05 +0100

    first rev of project

--- Postcard Amsterdam/ViewController.swift
+++ Postcard Amsterdam/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var sentButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        sentButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+    }
 
 }
-
+//1.45.27 daar ben ik!
